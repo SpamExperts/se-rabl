08de526aad23f45cc9e36224c8fec80f4ac3354c
Tony Meyer <tony@spamexperts.com>
2016-10-17 17:34:49 +1300

    Improve logging.

--- rabl/rabl.py
+++ rabl/rabl.py
@@ -41,6 +41,12 @@ import raven.transport
 from raven.handlers.logging import SentryHandler
 
 
+SENTRY_IGNORES = [
+    "KeyboardInterrupt",
+    "MemoryError",
+]
+
+
 def load_configuration():
     """Load server-specific configuration settings."""
     conf = ConfigParser.ConfigParser()
@@ -163,6 +169,47 @@ class RABLServer(SocketServer.UDPServer):
         SocketServer.UDPServer.__init__(self, address, self.handler_class)
 
 
+def setup_logging(logger, filename=None, sentry=None, application=None,
+                  stream_level=None):
+    """Initialize logging for this logger.
+
+    :param logger: The logger that should be initialized.
+    :param filename: The filename.
+    :param sentry: If specified also add a SentryHandler
+    :param stream_level: If specified add a stream handler
+      and set it to that level
+    """
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    logger.setLevel(logging.DEBUG)
+
+    if filename:
+        file_handler = logging.FileHandler(filename)
+        file_handler.setFormatter(formatter)
+        file_handler.setLevel(logging.INFO)
+        logger.addHandler(file_handler)
+
+    if stream_level is not None:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(getattr(logging, stream_level))
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+
+    if sentry:
+        client = raven.Client(sentry,
+                              enable_breadcrumbs=False,
+                              ignore_exceptions=SENTRY_IGNORES,
+                              transport=raven.transport.HTTPTransport)
+        sentry_handler = SentryHandler(client)
+        sentry_handler.setLevel(logging.WARNING)
+        logger.addHandler(sentry_handler)
+        null_loggers = [
+            logging.getLogger("sentry.errors"),
+            logging.getLogger("sentry.errors.uncaught")
+        ]
+        for null_logger in null_loggers:
+            null_logger.handlers = [logging.NullHandler()]
+
+
 def main():
     """Parse command-line options and execute requested actions."""
     description = "Reactive Autonomous Black List"
@@ -185,29 +232,18 @@ def main():
         proc.set_ionice(options.ionice, options.ionice_prio)
 
     logger = logging.getLogger("rabl")
-    logger.setLevel(logging.DEBUG)
     if options.debug:
-        handler = logging.StreamHandler()
-        handler.setLevel(logging.DEBUG)
+        stream_level = "DEBUG"
     else:
-        daemonize(
-            stdout="/var/log/rabl_server.out",
-            pidfile="/var/run/rabl_server.pid",
-            startmsg="%s")
-        handler = logging.FileHandler(
-            "/var/log/rabl_server.log")
-        handler.setLevel(logging.WARNING)
-    if CONF.get("sentry", "dsn"):
-        client = raven.Client(CONF.get("sentry", "dsn"),
-                              transport=raven.transport.HTTPTransport)
-        sentry_handler = SentryHandler(client)
-        sentry_handler.setLevel(logging.WARNING)
-        logger.addHandler(sentry_handler)
-        sentry_internal = logging.getLogger("sentry.errors")
-        sentry_internal.addHandler(sentry_handler)
-    handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s %(message)s'))
-    logger.addHandler(handler)
+        stream_level = "CRITICAL"
+    setup_logging(logger, "/var/log/rabl_server.log",
+                  CONF.get("sentry", "dsn"),
+                  stream_level=stream_level)
+
+    daemonize(
+        stdout="/var/log/rabl_server.out",
+        pidfile="/var/run/rabl_server.pid",
+        startmsg="%s")
     server = RABLServer(("", options.port))
     server.serve_forever()
 
