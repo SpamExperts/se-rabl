2e7bf14352f39de5d0b47ac5765b1e1f49d0e84e
Mugur Anghel <mugur.anghel@nable.com>
2026-03-06 17:55:17 +0200

    Git is optional, just write alter table in prod

--- sql/dnsbl.sql
+++ sql/dnsbl.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `rabl-verified` (
   `ip` char(43) NOT NULL COMMENT "The reported IP address or network",
   `reporter` char(43) NOT NULL COMMENT "The IP of the reporting user",
   `spam_count` int(11) default 0 COMMENT "The number of spam messages seen from this IP",
-  `last_seen` timestamp COMMENT "Last time a spam message was seen from this IP by this user",
+  `last_seen` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Last time a spam message was seen from this IP by this user'
   PRIMARY KEY  (`ip`, `reporter`),
   KEY  (`ip`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT="Reactive Autonomous Blackhole List";
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS `rabl-reported` (
   `ip` char(43) NOT NULL COMMENT "The reported IP address or network",
   `reporter` char(43) NOT NULL COMMENT "The IP of the reporting user",
   `spam_count` int(11) default 0 COMMENT "The number of spam messages seen from this IP",
-  `last_seen` timestamp COMMENT "Last time a spam message was seen from this IP by this user",
+  `last_seen` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Last time a spam message was seen from this IP by this user',
   PRIMARY KEY  (`ip`, `reporter`),
   KEY  (`ip`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT="Reactive Autonomous Blackhole List";
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS `rabl-automatic` (
   `ip` char(43) NOT NULL COMMENT "The reported IP address or network",
   `reporter` char(43) NOT NULL COMMENT "The IP of the reporting user",
   `spam_count` int(11) default 0 COMMENT "The number of spam messages seen from this IP",
-  `last_seen` timestamp COMMENT "Last time a spam message was seen from this IP by this user",
+  `last_seen` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Last time a spam message was seen from this IP by this user',
   PRIMARY KEY  (`ip`, `reporter`),
   KEY  (`ip`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT="Reactive Autonomous Blackhole List";
