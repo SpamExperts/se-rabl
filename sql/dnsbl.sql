ecd868849b9119c64013e508fad7b8ecacf4610c
Mugur Anghel <mugur.anghel@nable.com>
2026-03-10 16:04:36 +0200

    Schema updates

--- sql/dnsbl.sql
+++ sql/dnsbl.sql
@@ -1,26 +1,26 @@
 CREATE TABLE IF NOT EXISTS `rabl-verified` (
   `ip` char(43) NOT NULL COMMENT "The reported IP address or network",
   `reporter` char(43) NOT NULL COMMENT "The IP of the reporting user",
-  `spam_count` int(11) default 0 COMMENT "The number of spam messages seen from this IP",
+  `spam_count` int(11) NOT NULL DEFAULT 0, COMMENT "The number of spam messages seen from this IP",
   `last_seen` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Last time a spam message was seen from this IP by this user'
   PRIMARY KEY  (`ip`, `reporter`),
   KEY  (`ip`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT="Reactive Autonomous Blackhole List";
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT="Reactive Autonomous Blackhole List";
 
 CREATE TABLE IF NOT EXISTS `rabl-reported` (
   `ip` char(43) NOT NULL COMMENT "The reported IP address or network",
   `reporter` char(43) NOT NULL COMMENT "The IP of the reporting user",
-  `spam_count` int(11) default 0 COMMENT "The number of spam messages seen from this IP",
+  `spam_count` int(11) NOT NULL DEFAULT 0, COMMENT "The number of spam messages seen from this IP",
   `last_seen` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Last time a spam message was seen from this IP by this user',
   PRIMARY KEY  (`ip`, `reporter`),
   KEY  (`ip`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT="Reactive Autonomous Blackhole List";
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT="Reactive Autonomous Blackhole List";
 
 CREATE TABLE IF NOT EXISTS `rabl-automatic` (
   `ip` char(43) NOT NULL COMMENT "The reported IP address or network",
   `reporter` char(43) NOT NULL COMMENT "The IP of the reporting user",
-  `spam_count` int(11) default 0 COMMENT "The number of spam messages seen from this IP",
+  `spam_count` int(11) NOT NULL DEFAULT 0, COMMENT "The number of spam messages seen from this IP",
   `last_seen` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Last time a spam message was seen from this IP by this user',
   PRIMARY KEY  (`ip`, `reporter`),
   KEY  (`ip`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT="Reactive Autonomous Blackhole List";
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT="Reactive Autonomous Blackhole List";
